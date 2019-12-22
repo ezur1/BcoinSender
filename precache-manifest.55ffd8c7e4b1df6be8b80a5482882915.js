6eae8ffee7e20a489678b0be7bb5a65b5fefb152
Elior <ezur6294@gmail.com>
2019-12-22 11:10:39 +0200

    Updates

---
+++ precache-manifest.55ffd8c7e4b1df6be8b80a5482882915.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "733bbe4c454f536e5b5eb3f369f4b3c4",
+    "url": "​https://ezur1.github.io/BcoinSender/index.html"
+  },
+  {
+    "revision": "e48baa8d484f84470296",
+    "url": "​https://ezur1.github.io/BcoinSender/static/css/main.4ad1cd6b.chunk.css"
+  },
+  {
+    "revision": "d4415d1bc37fb09d7bbc",
+    "url": "​https://ezur1.github.io/BcoinSender/static/js/2.84acb293.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "​https://ezur1.github.io/BcoinSender/static/js/2.84acb293.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e48baa8d484f84470296",
+    "url": "​https://ezur1.github.io/BcoinSender/static/js/main.d9841986.chunk.js"
+  },
+  {
+    "revision": "bfbe6cada88b9aa417aa",
+    "url": "​https://ezur1.github.io/BcoinSender/static/js/runtime-main.37276a11.js"
+  },
+  {
+    "revision": "db54ac56949da5b4ed1b8c40afb19daf",
+    "url": "​https://ezur1.github.io/BcoinSender/static/media/bgc2.db54ac56.jpg"
+  },
+  {
+    "revision": "c087b1b65153c8d2b109e2eaebbcc9ce",
+    "url": "​https://ezur1.github.io/BcoinSender/static/media/logo.c087b1b6.svg"
+  }
+]);
